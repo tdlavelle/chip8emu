10292bd3ff8ba2d11a0a5ee341c3deeec856176e
Thomas Lavelle <tdlavelle@gmail.com>
2021-09-06 17:40:53 -0400

    cleaned up code to prep for implementing execution

--- src/chip8emu.c
+++ src/chip8emu.c
@@ -2,51 +2,46 @@
 #include <string.h>
 
 #define MEM_SIZE 4096
-long int loadRom(char* rom_in, unsigned char* ram_out);
-void disassemble(char* rom_in, char* file_out);
+#define START_ADDR 0x200
+
+long int loadRom(char* rom_in, unsigned char* ram_out, long int start_addr, long int max_size);
+void execute(char* rom_in, int disFlag);
 
 void main(int argc, char**argv)
 {
     // process command line options
-    // -d <rom_file> <output_file>: disassemble <rom_file> to <output_file>
-    // -e <rom_file>: execute <rom_file>
-    if (argc > 1)
+    // [-d] <rom_file>: Execute ROM, if -d is present then disassemble instead 
+    if (argc == 3 && !strcmp(argv[1], "-d"))
     {
-        printf("argv[1] = %s\nargc = %d\n", argv[1], argc);
-
         // disassemble option
-        if (!strcmp(argv[1], "-d") && argc == 4)
-        {
-            printf("disassembling %s, writing to %s\n", argv[2], argv[3]);
-            disassemble(argv[2], argv[3]);
-        }
-        else
-        {
-            printf("Incorrect arguments: -d <rom_file> <output_file>: disassemble <rom_file> to <output_file>\n");
-        }
+        execute(argv[2], 1);
+    }
+    else if (argc == 2)
+    {
+        // execute
+        execute(argv[1], 0);
     }
     else
     {
-        printf("hello, world\n");
+         printf("Usage: %s [-d] <rom_file>: specify -d to disassemble instead of execute.\n", argv[0]);
     }
 }
 
-void disassemble(char* rom_in, char* file_out)
+void execute(char* rom_in, int disFlag)
 {
-    // read in rom
-    unsigned char ram[MEM_SIZE]; // should probably dynamically allocate
+    // initialize ram
+    unsigned char ram[MEM_SIZE];
     for (int i = 0; i < MEM_SIZE; i++)
-    {
-        ram[i] = 0;
-    }	
+        ram[i] = 0;	
     
+    // load rom
     long int rom_size = 0;
-    rom_size = loadRom(rom_in, ram); 
+    rom_size = loadRom(rom_in, ram, START_ADDR, MEM_SIZE); 
+    
     // fetch/decode/execute
-
-    for (int pc = 0x200; pc < rom_size+0x200; pc++)
+    for (int pc = START_ADDR; pc < rom_size+START_ADDR; pc=pc+2)
     {
-        unsigned short inst = (((unsigned short)ram[pc]) << 8) | ((unsigned short)ram[++pc]);
+        unsigned short inst = (((unsigned short)ram[pc]) << 8) | ((unsigned short)ram[pc+1]);
 
         unsigned short opcode = (inst&0xF000)>>12;
         unsigned short regX = (inst&0x0F00)>>8;
@@ -55,106 +50,185 @@ void disassemble(char* rom_in, char* file_out)
         unsigned short valNN = (inst&0x00FF);
         unsigned short valNNN = (inst&0x0FFF);
 
-        printf("%4x [%04x]: ", (pc-1), inst);
+        if (disFlag) printf("%4x [%04x]: ", pc, inst);
         switch (opcode)
         {
             case 0x0:
                 switch (valNN)
                 {
                     case 0xE0:
-                        printf("CLS");
+                        if (disFlag) printf("CLS");
+                        else
+                        {
+                        }
                         break;
 
                     case 0xEE:
-                        printf("RET");
+                        if (disFlag) printf("RET");
+                        else
+                        {
+                        }
                         break;
 
                     default: // 0x0nnn
-                        printf("SYS %03x", valNNN);
+                        if (disFlag) printf("SYS %03x", valNNN);
+                        // not implementing
                         break;
                 }
                 break;
             case 0x1:
-                printf("JP %03x", valNNN);
+                if (disFlag) printf("JP %03x", valNNN);
+                else
+                {
+                }
                 break;
             case 0x2:
-                printf("CALL %03x", valNNN);
+                if (disFlag) printf("CALL %03x", valNNN);
+                else
+                {
+                }
                 break;
             case 0x3:
-                printf("SE V%x, %02x", regX, valNN);
+                if (disFlag) printf("SE V%x, %02x", regX, valNN);
+                else
+                {
+                }
                 break;
             case 0x4:
-                printf("SNE V%x, %02x", regX, valNN);
+                if (disFlag) printf("SNE V%x, %02x", regX, valNN);
+                else
+                {
+                }
                 break;
             case 0x5:
-                printf("SE V%x, V%x", regX, regY);
+                if (disFlag) printf("SE V%x, V%x", regX, regY);
+                else
+                {
+                }
                 break;
             case 0x6:
-                printf("LD V%x, %02x", regX, valNN);
+                if (disFlag) printf("LD V%x, %02x", regX, valNN);
+                else
+                {
+                }
                 break;
             case 0x7:
-                printf("ADD V%x, %02x", regX, valNN);
+                if (disFlag) printf("ADD V%x, %02x", regX, valNN);
+                else
+                {
+                }
                 break;
             case 0x8:
                 switch (valN)
                 {
                     case 0x0:
-                        printf("LD V%x, V%x", regX, regY);
+                        if (disFlag) printf("LD V%x, V%x", regX, regY);
+                        else
+                        {
+                        }
                         break;
                     case 0x1:
-                        printf("OR V%x, V%x", regX, regY);
+                        if (disFlag) printf("OR V%x, V%x", regX, regY);
+                        else
+                        {
+                        }
                         break;
                     case 0x2:
-                        printf("AND V%x, V%x", regX, regY);
+                        if (disFlag) printf("AND V%x, V%x", regX, regY);
+                        else
+                        {
+                        }
                         break;
                     case 0x3:
-                        printf("XOR V%x, V%x", regX, regY);
+                        if (disFlag) printf("XOR V%x, V%x", regX, regY);
+                        else
+                        {
+                        }
                         break;
                     case 0x4:
-                        printf("ADD V%x, V%x", regX, regY);
+                        if (disFlag) printf("ADD V%x, V%x", regX, regY);
+                        else
+                        {
+                        }
                         break;
                     case 0x5:
-                        printf("SUB V%x, V%x", regX, regY);
+                        if (disFlag) printf("SUB V%x, V%x", regX, regY);
+                        else
+                        {
+                        }
                         break;
                     case 0x6:
-                        printf("SHR V%x {, V%x}", regX, regY);
+                        if (disFlag) printf("SHR V%x {, V%x}", regX, regY);
+                        else
+                        {
+                        }
                         break;
                     case 0x7:
-                        printf("SUBN V%x, V%x", regX, regY);
+                        if (disFlag) printf("SUBN V%x, V%x", regX, regY);
+                        else
+                        {
+                        }
                         break;
                     case 0xE:
-                        printf("SHL V%x {, V%x}", regX, regY);
+                        if (disFlag) printf("SHL V%x {, V%x}", regX, regY);
+                        else
+                        {
+                        }
                         break;
                     default:
-                        printf("not supported");
+                        if (disFlag) printf("not supported");
                 }
                 break;
             case 0x9:
-                printf("SNE V%x, V%x", regX, regY);
+                if (disFlag) printf("SNE V%x, V%x", regX, regY);
+                else
+                {
+                }
                 break;
             case 0xA:
-                printf("LD I, %03x", valNNN);
+                if (disFlag) printf("LD I, %03x", valNNN);
+                else
+                {
+                }
                 break;
             case 0xB:
-                printf("JP V0, %03x", valNNN);
+                if (disFlag) printf("JP V0, %03x", valNNN);
+                else
+                {
+                }
                 break;
             case 0xC:
-                printf("RND V%x, %02x", regX, valNN);
+                if (disFlag) printf("RND V%x, %02x", regX, valNN);
+                else
+                {
+                }
                 break;
             case 0xD:
-                printf("DRW V%x, V%x, %x", regX, regY, valN);
+                if (disFlag) printf("DRW V%x, V%x, %x", regX, regY, valN);
+                else
+                {
+                }
                 break;
             case 0xE:
                 switch (valNN)
                 {
                     case 0x9E:
-                        printf("SKP V%x", regX);
+                        if (disFlag) printf("SKP V%x", regX);
+                        else
+                        {
+                        }
                         break;
                     case 0xA1:
-                        printf("SKNP V%x", regX);
+                        if (disFlag) printf("SKNP V%x", regX);
+                        else
+                        {
+                        }
                         break;
                     default:
-                        printf("not supported");
+                        if (disFlag) printf("not supported");
+                        else
+                        {
+                        }
                         break;
                 }
                 break;
@@ -162,43 +236,73 @@ void disassemble(char* rom_in, char* file_out)
                 switch (valNN)
                 {
                     case 0x07:
-                        printf("LD V%x, DT", regX);
+                        if (disFlag) printf("LD V%x, DT", regX);
+                        else
+                        {
+                        }
                         break;
                     case 0x0A:
-                        printf("LD V%x, K", regX);
+                        if (disFlag) printf("LD V%x, K", regX);
+                        else
+                        {
+                        }
                         break;
                     case 0x15:
-                        printf("LD DT, V%x", regX);
+                        if (disFlag) printf("LD DT, V%x", regX);
+                        else
+                        {
+                        }
                         break;
                     case 0x18:
-                        printf("LD ST, V%x", regX);
+                        if (disFlag) printf("LD ST, V%x", regX);
+                        else
+                        {
+                        }
                         break;
                     case 0x1E:
-                        printf("ADD I, V%x", regX);
+                        if (disFlag) printf("ADD I, V%x", regX);
+                        else
+                        {
+                        }
                         break;
                     case 0x29:
-                        printf("LD F, V%x", regX);
+                        if (disFlag) printf("LD F, V%x", regX);
+                        else
+                        {
+                        }
                         break;
                     case 0x33:
-                        printf("LD B, V%x", regX);
+                        if (disFlag) printf("LD B, V%x", regX);
+                        else
+                        {
+                        }
                         break;
                     case 0x55:
-                        printf("LD [I], V%x", regX);
+                        if (disFlag) printf("LD [I], V%x", regX);
+                        else
+                        {
+                        }
                         break;
                     case 0x65:
-                        printf("LD V%x, [I]", regX);
+                        if (disFlag) printf("LD V%x, [I]", regX);
+                        else
+                        {
+                        }
+                        break;
+                    default:
+                        if (disFlag) printf("not supported");
                         break;
                 }
                 break;
             default:
-                printf("not supported");
+                if (disFlag) printf("not supported");
                 break;
         }
-        printf("\n");
+        if (disFlag) printf("\n");
     }
 }
 
-long int loadRom(char* rom_in, unsigned char* ram_out)
+long int loadRom(char* rom_in, unsigned char* ram_out, long int start_addr, long int max_size)
 {
     // open file
     FILE *romPtr = fopen(rom_in, "rb");
@@ -212,17 +316,16 @@ long int loadRom(char* rom_in, unsigned char* ram_out)
     fseek(romPtr, 0, SEEK_END);
     rom_size = ftell(romPtr);
     rewind(romPtr);
-    if (rom_size > (MEM_SIZE-0x200))
+    if (rom_size > (max_size-start_addr))
     {
-	    printf("ROM is larger than RAM, only reading first %d bytes", MEM_SIZE);
-	    rom_size = (MEM_SIZE-0x200);
+	    printf("ROM is larger than RAM, only reading first %d bytes", max_size);
+	    rom_size = (max_size-start_addr);
     }
 
     //read in
-    for (int i = 0x200; i < (rom_size+0x200); i++)
+    for (int i = start_addr; i < (rom_size+start_addr); i++)
     {
         fread(&ram_out[i], sizeof(unsigned char), 1, romPtr);
-        //printf("%x: %x\n", i, ram_out[i]);
     }
 
     fclose(romPtr);
